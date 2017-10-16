e6362e6e3a40f241bfe7a03c52123ed92d716826
Nobuki Yoda <nobuki.y@gmail.com>
2017-10-16 17:18:37 +0000

    Added error messages.

--- atcoder/arc001/a-center.rs
+++ atcoder/arc001/a-center.rs
@@ -3,12 +3,12 @@ use std::error::Error;
 
 fn run() -> Result<(), Box<Error>> {
     let mut n_str = String::new();
-    io::stdin().read_line(&mut n_str)?;
-    let n = n_str.trim().parse::<usize>()?;
+    io::stdin().read_line(&mut n_str).or(Err("no n"))?;
+    let n = n_str.trim().parse::<usize>().or(Err("parse error: n"))?;
 
     let mut count: [i32; 4] = [0; 4];
     let mut cs = String::new();
-    io::stdin().read_line(&mut cs)?;
+    io::stdin().read_line(&mut cs).or(Err("no cs"))?;
     for c in cs.as_bytes()[0..n].iter() {
         let i = (c - ('1' as u8)) as usize;
         count[i] += 1
